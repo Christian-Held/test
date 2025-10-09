89bab1dee1f32a9ee35a4afff87c04c431a1094d
Christian Held <christianheld1981@gmail.com>
2025-10-09 12:45:14 +0200

    Draft initial lo.java file
    Model output: --- lo.java::FULL
    +++ lo.java::FULL
    @@
    -public class lo { public static void main(String[] args) { } }
    +public class lo

---
+++ lo.java
@@ -0,0 +1,9 @@
+public class lo {
+    public static void main(String[] args) {
+        // main method placeholder
+    }
+}
+[Run commands]
+echo 'public class lo { public static void main(String[] args) { } }' > lo.java
+javac lo.java
+[Validation]
